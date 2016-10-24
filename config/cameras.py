39e17ccbf18d288ac188f65e0d285f24dce864ca
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2016-10-24 10:29:47 +0100

    adeded Zyla cameras to config

--- config/cameras.py
+++ config/cameras.py
@@ -2,6 +2,9 @@
 cameras = [
     ('West', 9146, 1<<8, '192.168.1.2', 7777, 'ixon', ['TRITC'], [600], (0,0,1)),
     ('East', 9145, 1<<9, '192.168.1.2', 7778, 'ixon', ['GFP'], [525], (0,0,1)),
+    ("North", 'VSC-01344', 0, '192.168.1.2',8192, 'universal', ['ZYLA'], [525],
+    (0, 0, 1)),
+    ("South", 0000, 0, '192.168.1.2', 8193, 'universal', ['TEST'], [615]),
     ]
 camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths','baseTransform']
 #transform order [0]=rot90,[1]=flip_h,[2]=flip_v
