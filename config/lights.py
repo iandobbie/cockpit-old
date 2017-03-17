9a63c634ae53896acb3ee5832f87acc05dadacd4
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2017-03-17 15:14:29 +0000

    Fixed light definitions for OMXv2

--- config/lights.py
+++ config/lights.py
@@ -5,15 +5,14 @@ WAVELENGTH_TO_COLOR = {
     488: (40, 130, 180),
     532: (80, 255, 0),
     561: (176, 255, 0),
-    594: (255,200,0),
+    593: (255,200,0),
     640: (255, 40, 40),
     'white': (255, 255, 255)
 }
-
 lights = [
     ('ambient', 'Ambient', WAVELENGTH_TO_COLOR['white'], 0),
-    ('405nm', 405, WAVELENGTH_TO_COLOR[405], 1<<13),
-    ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<9),
-    ('532nm', 532, WAVELENGTH_TO_COLOR[532], 1<<14),
-    ('593nm', 593, WAVELENGTH_TO_COLOR[593], 1<<12),
-    ('DIC', 'DIC', WAVELENGTH_TO_COLOR['white'], 1<<15),]
+    ('405nm', 405, WAVELENGTH_TO_COLOR[405], 1<<13,),
+    ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<9,),
+    ('532nm', 532, WAVELENGTH_TO_COLOR[532], 1<<14,),
+    ('593nm', 593, WAVELENGTH_TO_COLOR[593], 1<<12,),
+    ('DIC', 'DIC', WAVELENGTH_TO_COLOR['white'], 1<<15,),]
