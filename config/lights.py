c568adaa1542029915812e5ecf1201b4b5a796bc
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2017-03-17 14:37:32 +0000

    edited lights.py to reflect lines that V2 laser are on

--- config/lights.py
+++ config/lights.py
@@ -3,14 +3,17 @@ light_keys = ['label','wavelength', 'color', 'triggerLine','simtheta','port','de
 WAVELENGTH_TO_COLOR = {
     405: (180, 30, 230),
     488: (40, 130, 180),
+    532: (80, 255, 0),
     561: (176, 255, 0),
+    594: (255,200,0),
     640: (255, 40, 40),
     'white': (255, 255, 255)
 }
 
 lights = [
     ('ambient', 'Ambient', WAVELENGTH_TO_COLOR['white'], 0),
-#    ('405nm', 405, WAVELENGTH_TO_COLOR[405], 1<<13, 10),
-#    ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<9, 9, 7776, 'deepstar'),
-#    ('561nm', 561, WAVELENGTH_TO_COLOR[561], 1<<13, 8),
-    ('DIC', 'DIC', WAVELENGTH_TO_COLOR['white'], 1<<11),]
+    ('405nm', 405, WAVELENGTH_TO_COLOR[405], 1<<13),
+    ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<9),
+    ('532nm', 532, WAVELENGTH_TO_COLOR[532], 1<<14),
+    ('593nm', 593, WAVELENGTH_TO_COLOR[593], 1<<12),
+    ('DIC', 'DIC', WAVELENGTH_TO_COLOR['white'], 1<<15),]
