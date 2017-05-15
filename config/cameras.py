2de86b2341d4f46150378dc02dc000cc1f6e2ea5
langefj <frederik.lange@dtc.ox.ac.uk>
2017-05-15 17:13:04 +0100

    Updated cameras.py config file to use changes made to UniversalCamera class

--- config/cameras.py
+++ config/cameras.py
@@ -1,5 +1,5 @@
 cameras = [
-    ('PVCamera','West', 0000, 1<<0, '10.0.0.10', 8001, 'universalCamera', ['GFP'], [525])
+    ('West', 0000, 1<<0, '10.0.0.10', 8001, 'PVCamera', ['GFP'], [525])
     #('East', 9145, 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
 ]
-camera_keys = ['DeviceServer','label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
+camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
