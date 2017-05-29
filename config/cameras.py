9dee00712f4bc9cbed046d0eea74a7bdbac8c782
langefj <frederik.lange@dtc.ox.ac.uk>
2017-05-29 12:38:48 +0100

    Changed from PVCamera to AndorSDK3 in config/cameras.py due to hardware change. Note that this is now running on the machine local to cockpit

--- config/cameras.py
+++ config/cameras.py
@@ -1,5 +1,5 @@
 cameras = [
-    ('West', 0000, 1<<0, '10.0.0.10', 8001, 'PVCamera', ['GFP'], [525])
+    ('West', 01605, 1<<8, '127.0.0.1', 8001, 'AndorSDK3', ['GFP'], [525]),
     #('East', 9145, 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
 ]
 camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
